09fa11b1c4653911e7228a604550e832adac7c08
JulesBernal <102987740+JulesBernal@users.noreply.github.com>
2022-07-26 23:42:44 -0400

    jank

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 app.use(bodyParser.urlencoded({extended:true}));
 
 const connection = mysql.createConnection({
