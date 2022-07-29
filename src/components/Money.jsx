d316921463faa33404a582f7f5fffaad8c9359ba
JulesBernal <102987740+JulesBernal@users.noreply.github.com>
2022-07-29 13:35:49 -0400

    some

--- src/components/Money.jsx
+++ src/components/Money.jsx
@@ -6,17 +6,18 @@ export default function Money(props) {
 const { register, handleSubmit} = useForm();
 
   const onSubmit = formData => {
-    console.log(parseFloat(formData.denom));
-    props.coin(parseFloat(formData.denom));
+    // console.log(parseFloat(formData.denom));
+    // props.coin(parseFloat(formData.denom));
+    props.coin((formData.denom))
   };
   return (
     <div>
       <form onChange={handleSubmit(onSubmit)}>
        <select {...register("denom")}  >
-          <option value="1">USD</option>
-          <option value="134">YEN</option>
-          <option value="0.0000416">BTC</option>
-          <option value="14">DOGE</option>
+          <option value="$">USD</option>
+          <option value="¥">YEN</option>
+          <option value="₿">BTC</option>
+          <option value="Ð">DOGE</option>
         </select>
       </form>
     </div>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,14 +9,17 @@ export default function Product(props) {
   }
   const price = parseFloat(fruitData.price) * props.coin 
   const flatValue= props.coin===0.0000416 ? parseFloat(price).toFixed(4) : price;
-  // const currencyStamp= {"0.0000416":"₿","" :"$", :"¥",:"Ð"}
+  const currencyStamp= {"₿":0.0000416,"$":1,"¥":134,"Ð":14}
+  console.log(currencyStamp[props.coin])
   return (
     <figure className="fruit">
         <p>{fruitData.fruit_type}</p>
         <img src={fruitData.img} alt={alt}/>
         <p>{fruitData.title} Fruit</p>
         <p>{fruitData.description}</p>
-        <p>USD ${price ? parseFloat(flatValue).toFixed(4) : fruitData.price}</p>
+
+        <p>{props.coin ? props.coin : '$'} {props.coin ? parseFloat(currencyStamp[props.coin]*fruitData.price).toFixed(4) : fruitData.price}</p>
+        {/* <p>USD ${price ? parseFloat(flatValue).toFixed(4) : fruitData.price}</p> */}
         <button onClick={addCart}>Add to Cart</button>
     </figure>
   )
